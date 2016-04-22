d4c71d5ce048f7b9f8cca57750103eb1649c75ff
Yonah Forst <Yonah@Yonahs-MacBook-Pro-2.local>
2016-04-22 18:02:06 +0200

    added safe mode

--- content_script.js
+++ content_script.js
@@ -1,11 +1,18 @@
 "use strict";
 
 const SNOOZES = [
-  'fuck off', 
   'later..', 
   "leave me alone", 
   'go bother someone else',
-  'five more minutes',
+  'a few more minutes',
+  'snooze button',
+  'zzzz....',
+  '🙄😴',
+]
+
+const UNSAFE_SNOOZES = [
+  'fuck off',
+  '🖕🏼',
 ]
 
 const QUESTIONS = [
@@ -16,66 +23,42 @@ const QUESTIONS = [
   "Fine, just tell me why you needed to open facebook",
 ]
 
+const UNSAFE_QUESTIONS = []
+
 var app = {
   hiddenCount: 0,
   didChangeTaskText: false,
+  snoozes: SNOOZES,
+  questions: QUESTIONS,
+  safeMode: false,
 }
-app.topContainerElement = document.createElement('div');
-app.topContainerElement.setAttribute('id', 'focusbookTopContainer');
-app.topContainerElement.setAttribute('class', 'focusbookActive');
-app.topContainerElement.style.top = '-300px'
 
-//MAIN CONTAINER
-app.mainContainerElement = document.createElement('div');
-app.mainContainerElement.setAttribute('id', 'focusbookMainContainer');
-
-app.textElement = document.createElement("span");
-app.textElement.innerHTML = 
-app.textElement.innerHTML = QUESTIONS[Math.floor(Math.random()*QUESTIONS.length)];
-app.textElement.setAttribute('id', 'focusbookMainText');
-
-app.inputElement = document.createElement("input")
-app.inputElement.setAttribute('id', 'focusbookInput');
-app.inputElement.setAttribute('type', 'text');
+chrome.storage.sync.get({
+  safemode: false,
+}, function(items) {
+  app.safeMode = items.safemode;
+  if (!app.safeMode) {
+    app.snoozes = [...app.snoozes, UNSAFE_SNOOZES];
+    app.questions = [...app.questions, UNSAFE_QUESTIONS];
+  } 
+});
 
 
-app.inputElement.onkeypress = function(e){
-    if (!e) e = window.event;
-    var keyCode = e.keyCode || e.which;
-    if (keyCode == '13'){
-      hideTask();
-      startTask(this.value)
-      return false;
-    }
-  }
 
-app.mainContainerElement.appendChild(app.textElement);
-app.mainContainerElement.appendChild(app.inputElement);
-
-
-// BUTTONS CONTAINER
-app.buttonsContainerElement = document.createElement('div');
-app.buttonsContainerElement.setAttribute('id', 'focusbookButtons');
-
-app.snoozeButton = document.createElement('button');
-app.snoozeButton.setAttribute('id', 'snoozeButton');
-app.snoozeButton.setAttribute('class', 'focusbookButton')
-app.snoozeButton.onclick = hideTask;
-
-app.buttonsContainerElement.appendChild(app.snoozeButton);
 
 function showTask() {
   app.topContainerElement.setAttribute('class', app.task ? 'focusbookPassive' : 'focusbookActive')
   app.topContainerElement.style.top = '0px'
-  app.snoozeButton.innerHTML = SNOOZES[Math.floor(Math.random()*SNOOZES.length)];
+  app.snoozeButton.innerHTML = app.snoozes[Math.floor(Math.random()*app.snoozes.length)];
   app.expandTimer = setTimeout(expandContainer,  10000);
 }
 
 function hideTask() {
   app.topContainerElement.setAttribute('class', 'focusbookPassive')
   app.topContainerElement.style.top = '-300px'
-  app.hiddenCount ++
+  app.hiddenCount++
   app.timer = setTimeout(showTask,  20000 + 10000 * app.hiddenCount * app.hiddenCount);
+  clearTimeout(app.expandTimer)
 }
 
 function expandContainer() {
@@ -84,75 +67,88 @@ function expandContainer() {
 
 function startTask(taskString) {
   app.task = taskifyString(taskString)
-  app.didChangeTaskText = app.task != taskString
-  if (app.didChangeTaskText) {
-    app.textElement.innerHTML = "Finished " + app.task + "? GET BACK TO FUCKING WORK!";
+
+  if (app.task != taskString) {
+    app.textElement.innerHTML = `Finished ${app.task}? GET BACK TO ${!app.safeMode ? "FUCKING " : ""}WORK!`;
   } else {
     app.textElement.innerHTML = `'${app.task}' - finished yet? GO WORK!`;
   }
+
   app.inputElement.setAttribute('class', 'focusbookPassive')
   app.snoozeButton.style.visibility = "visible";
 }
 
 function taskifyString(string) {
-  newString = string.toLowerCase();
+  let newString = string.toLowerCase();
   newString = newString.replace(" my ", " your ");
   let array = newString.split(" ")
 
   if (array[0].toLowerCase() == 'to') {
     array.shift()
     array[0] = array[0] + 'ing';
+  } else if (array[0].slice(-3) == 'ing') {
+    array.unshift("'");
+    array.push("'")
   }
 
   return array.join(' ');
-
 }
 
-app.topContainerElement.appendChild(app.mainContainerElement);
-app.topContainerElement.appendChild(app.buttonsContainerElement);
+function onkeypress(e) {
+  if (!e) e = window.event;
+  var keyCode = e.keyCode || e.which;
+  if (keyCode == '13'){
+    startTask(this.value)
+    hideTask();
+    return false;
+  }
+}
 
-document.all[0].appendChild(app.topContainerElement);
+
+
+function createElement() {
+  app.topContainerElement = document.createElement('div');
+  app.topContainerElement.setAttribute('id', 'focusbookTopContainer');
+  app.topContainerElement.setAttribute('class', 'focusbookActive');
+  app.topContainerElement.style.top = '-300px'
+
+  //MAIN CONTAINER
+  app.mainContainerElement = document.createElement('div');
+  app.mainContainerElement.setAttribute('id', 'focusbookMainContainer');
+
+  app.textElement = document.createElement("span");
+  app.textElement.innerHTML = app.questions[Math.floor(Math.random()*app.questions.length)];
+  app.textElement.setAttribute('id', 'focusbookMainText');
+
+  app.inputElement = document.createElement("input")
+  app.inputElement.setAttribute('id', 'focusbookInput');
+  app.inputElement.setAttribute('type', 'text');
+  app.inputElement.onkeypress = onkeypress
+
+  app.mainContainerElement.appendChild(app.textElement);
+  app.mainContainerElement.appendChild(app.inputElement);
+
+
+  // BUTTONS CONTAINER
+  app.buttonsContainerElement = document.createElement('div');
+  app.buttonsContainerElement.setAttribute('id', 'focusbookButtons');
+
+  app.snoozeButton = document.createElement('button');
+  app.snoozeButton.setAttribute('id', 'snoozeButton');
+  app.snoozeButton.setAttribute('class', 'focusbookButton')
+  app.snoozeButton.onclick = hideTask;
+
+  app.buttonsContainerElement.appendChild(app.snoozeButton);
+  app.topContainerElement.appendChild(app.mainContainerElement);
+  app.topContainerElement.appendChild(app.buttonsContainerElement);
+
+  document.all[0].appendChild(app.topContainerElement);
+}
+
+createElement();
 showTask();
-// let container =  document.createElement("div");
-// container.setAttribute('id', 'focusbookTopContainer');
-// container.innerHTML = `
-// <div id='focusbookMainContainer' class='focusbookActive'>
-//   <span id='focusbookMainText'>
-//     What do you need to do?
-//   </span>
-//   <input id='focusbookInput' type='text'>
-//   </input>
-//   <span id='focusbookButtons'>
-//     <button id='snoozeButton' class='focusbookButton'>
-//       snooze
-//     </button>
-
-//     <button id='doneButton' class='focusbookButton'>
-//       done
-//     </button> 
-//   </span>
-// </div>
-// `
-// document.all[0].appendChild(container);
-
-// document.getElementById("focusbookInput").onkeypress = function(e){
-//     if (!e) e = window.event;
-//     var keyCode = e.keyCode || e.which;
-//     if (keyCode == '13'){
-//       startTask(this.value)
-//       return false;
-//     }
-//   };
 
 
 
-// function startTask(taskString) {
-//   let newString = taskString.replace(" my ", " your ")
-//   let text = document.getElementById("focusbookMainText")
-//   text.innerHTML = "GO "  + newString.toUpperCase()
-//   container.setAttribute('class', 'focusbookPassive')
-
-//   document.getElementById("focusbookInput").setAttribute('class', 'focusbookPassive')
-// }
 
 
